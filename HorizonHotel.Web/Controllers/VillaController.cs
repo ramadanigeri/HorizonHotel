5b7f59b68f180387ae27ab1d524f063dcc8edbc0
Geri Ramadani <ramadanigeri@gmail.com>
2023-12-12 15:40:15 +0100

    Create Villa endpoint, controller, table to fetch data from DB to Frontend + Fix Application from exeption handler not finding index page by installing RazorRuntimeCompliation

---
+++ HorizonHotel.Web/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using HorizonHotel.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HorizonHotel.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
--- HorizonHotel.Web/Program.cs
+++ HorizonHotel.Web/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 // Add Connection To SQL Server
 builder.Services.AddDbContext<ApplicationDbContext>(option=>
