1c42b4e513d566ec35034423039b314460766226
Geri Ramadani <ramadanigeri@gmail.com>
2023-12-14 17:47:27 +0100

    Add Resources + Enable Update & Delete Villa functionality

--- HorizonHotel.Web/Controllers/VillaController.cs
+++ HorizonHotel.Web/Controllers/VillaController.cs
@@ -38,5 +38,56 @@ namespace HorizonHotel.Web.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Update(int villaId)
+        {
+            // Method 1 to fetch data based on villa ID
+            Villa? obj = _db.Villas.Find(villaId);
+
+            // Method 2 to fetch data based on villa ID
+            // Villa? obj = _db.Villas.FirstOrDefault( u=> u.Id == villaId);
+
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.Find(villaId);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
